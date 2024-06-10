862800987edc5a4999d4fd22c589ca35fb722162
Nataren <20220594@ricaldone.edu.sv>
2024-06-09 19:09:43 -0600

    Primer commit

---
+++ app/src/main/java/N/J/L/F/S/Q/ignis_ptc/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package N.J.L.F.S.Q.ignis_ptc.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
