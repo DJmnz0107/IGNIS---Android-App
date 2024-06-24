2b49bcaf7dd0a92ac301c6f674f992c9658115f4
Djimenez <20200093@ricaldone.edu.sv>
2024-06-23 22:45:17 -0600

    Configuracion segundo bottom navigation

--- app/src/main/java/N/J/L/F/S/Q/ignis_ptc/activity_Login.kt
+++ app/src/main/java/N/J/L/F/S/Q/ignis_ptc/activity_Login.kt
@@ -1,10 +1,13 @@
 package N.J.L.F.S.Q.ignis_ptc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class activity_Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,24 @@ class activity_Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val randomNumber = Random.nextInt(2)
+
+            if (randomNumber == 0) {
+                val pantallaBombero = Intent(this, activity_bomberos::class.java)
+                startActivity(pantallaBombero)
+            }
+            else {
+                val pantallaMain = Intent(this, MainActivity::class.java)
+                startActivity(pantallaMain)
+            }
+
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/N/J/L/F/S/Q/ignis_ptc/activity_bomberos.kt
@@ -0,0 +1,38 @@
+package N.J.L.F.S.Q.ignis_ptc
+
+import N.J.L.F.S.Q.ignis_ptc.databinding.ActivityBomberosBinding
+import N.J.L.F.S.Q.ignis_ptc.databinding.ActivityMainBinding
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class activity_bomberos : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBomberosBinding
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bomberos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val navController = findNavController(R.id.nav_host_fragment_container)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_bomberos)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
