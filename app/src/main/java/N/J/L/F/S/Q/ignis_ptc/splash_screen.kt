e22644d5b473d8f0530ce667fd1c3818555f9e48
StevenDispox <20210240@ricaldone.edu.sv>
2024-07-10 07:27:39 -0600

    Splash screen terminada

---
+++ app/src/main/java/N/J/L/F/S/Q/ignis_ptc/splash_screen.kt
@@ -0,0 +1,36 @@
+package N.J.L.F.S.Q.ignis_ptc
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        GlobalScope.launch(Dispatchers.Main) {
+
+            //Esperar 3 segundos
+            delay(3000)
+            //Inicio la otra pantalla
+            val pantallaMain = Intent( this@splash_screen, MainActivity::class.java)
+            startActivity(pantallaMain)
+            finish()
+
+        }
+    }
+}
