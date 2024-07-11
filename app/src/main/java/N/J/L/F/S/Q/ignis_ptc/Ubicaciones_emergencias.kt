44e661291cb3f95455f4e511bff28bac5f8c0dac
Nataren <20220594@ricaldone.edu.sv>
2024-07-10 23:47:53 -0600

    fragment diseño y codigo Emergencias bomberos

--- app/src/main/java/N/J/L/F/S/Q/ignis_ptc/Ubicaciones_emergencias.kt
+++ app/src/main/java/N/J/L/F/S/Q/ignis_ptc/Ubicaciones_emergencias.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,13 +21,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Ubicaciones_Bomberos.newInstance] factory method to
+ * Use the [Ubicaciones_emergencias.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Ubicaciones_Bomberos : Fragment() {
+class Ubicaciones_emergencias : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,17 +39,25 @@ class Ubicaciones_Bomberos : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapEstaciones2) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val root = inflater.inflate(R.layout.fragment_ubicaciones__bomberos, container, false)
+        val root = inflater.inflate(R.layout.fragment_ubicaciones_emergencias, container, false)
 
-        val btnUbiEmergencia = root.findViewById<Button>(R.id.btnEmergenciaBombero)
+        val btnVolverEmergencias = root.findViewById<ImageView>(R.id.imgvolverEmergencias)
 
-        btnUbiEmergencia.setOnClickListener {
-            findNavController().navigate(R.id.haciaEmergencias)
+        btnVolverEmergencias.setOnClickListener {
+            findNavController().navigate(R.id.emergenciaAbomberos)
         }
         return root
     }
@@ -53,16 +69,37 @@ class Ubicaciones_Bomberos : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Ubicaciones_Bomberos.
+         * @return A new instance of fragment Ubicaciones_emergencias.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Ubicaciones_Bomberos().apply {
+            Ubicaciones_emergencias().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        createMarker()
+    }
+
+    private fun createMarker() {
+
+        val coordinates = LatLng(13.722845, -89.205214)
+
+        val marker = MarkerOptions().position(coordinates).title("El ITR")
+
+        map.addMarker(marker)
+
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f), 4000, null
+        )
+
     }
 }
